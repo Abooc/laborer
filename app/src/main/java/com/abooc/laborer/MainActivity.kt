5edb0ba349573573acda50602071065b3520096b
大宇 <allnet@live.cn>
2017-09-20 14:39:40 +0800

    增加：增加Kotlin支持；

---
+++ app/src/main/java/com/abooc/laborer/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.abooc.laborer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
